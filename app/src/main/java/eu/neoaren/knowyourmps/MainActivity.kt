379e8c3675d6e1f8e01e899c80575e1321cd461d
András Ádám <adm.andras@gmail.com>
2021-09-27 14:51:22 +0300

    Use retrofit and moshi to load MP data

--- app/src/main/java/eu/neoaren/knowyourmps/MainActivity.kt
+++ app/src/main/java/eu/neoaren/knowyourmps/MainActivity.kt
@@ -2,8 +2,11 @@ package eu.neoaren.knowyourmps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import eu.neoaren.knowyourmps.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,11 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    lifecycleScope.launch {
+      val mps = MemberOfParliamentApi.service.getMPs()
+      Log.d("MPs", mps.toString());
+    }
   }
 
 }
